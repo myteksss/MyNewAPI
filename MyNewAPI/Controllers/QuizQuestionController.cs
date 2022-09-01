3b553aee78616b1a8a22b94e5c8352019c8873d4
KRUK\mmytnik <michal.mytnik@kruksa.pl>
2022-09-01 18:21:02 +0200

    Added get method for question and class dto to add question

--- MyNewAPI/Controllers/QuizQuestionController.cs
+++ MyNewAPI/Controllers/QuizQuestionController.cs
@@ -21,5 +21,26 @@ namespace Quiz.API.Controllers
 
             return Ok(result.QuizQuestion);
         }
+
+        [HttpGet("GetQuestion/{quizId}/{questionId}")]
+        public ActionResult<QuizQuestionsDto> GetQuestion(int quizId, int questionId)
+        {
+            var quiz = QuizNameDataStore.Current.QuizNames.FirstOrDefault(x => x.Id == quizId);            
+            
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            var question = quiz.QuizQuestion.FirstOrDefault(x => x.Id == questionId);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(question);
+
+        }
     }
 }
---
+++ MyNewAPI/Models/AddQuizQuestionsDto .cs
@@ -0,0 +1,12 @@
+﻿using Quiz.API.Models;
+
+namespace Quiz.API.Models
+{
+    public class AddQuizQuestionsDto
+    {        
+        public int QuizNameId { get; set; }
+        public string Question { get; set; }
+        public int Level { get; set; }
+        public List<QuizOptionDto> quizOption { get; set; }       
+    }
+}
